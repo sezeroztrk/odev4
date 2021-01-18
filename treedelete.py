a01e4fdfd2cf3e84a427eed7f4452e27fcbc926d
sezeroztrk <74632404+sezeroztrk@users.noreply.github.com>
2021-01-18 22:58:22 +0300

    Update treedelete.py

--- treedelete.py
+++ treedelete.py
@@ -53,40 +53,26 @@ class Node:
 
 def minValueNode(node):
     current = node
- 
-    # loop down to find the leftmost leaf
+
     while(current.left is not None):
         current = current.left
  
     return current
  
-# Given a binary search tree and a key, this function
-# delete the key and returns the new root
- 
  
 def deleteNode(root, key):
  
-    # Base Case
     if root is None:
         return root
- 
-    # If the key to be deleted 
-    # is smaller than the root's
-    # key then it lies in  left subtree
+
     if key < root.data:
         root.left = deleteNode(root.left, key)
  
-    # If the kye to be delete 
-    # is greater than the root's key
-    # then it lies in right subtree
     elif(key > root.data):
         root.right = deleteNode(root.right, key)
  
-    # If key is same as root's key, then this is the node
-    # to be deleted
     else:
  
-        # Node with only one child or no child
         if root.left is None:
             temp = root.right
             root = None
@@ -97,20 +83,13 @@ def deleteNode(root, key):
             root = None
             return temp
  
-        # Node with two children: 
-        # Get the inorder successor
-        # (smallest in the right subtree)
         temp = minValueNode(root.right)
  
-        # Copy the inorder successor's 
-        # content to this node
         root.data = temp.key
- 
-        # Delete the inorder successor
+
         root.right = deleteNode(root.right, temp.key)
  
     return root
-# Use the insert method to add nodes
 
 root = Node(25)
 root.insert(12)
